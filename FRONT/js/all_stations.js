48d72718645a44e223ea1586b9d016c753f0f534
Adrian <adrianruizmora@hotmail.com>
2021-03-08 16:27:15 +0100

    front connecté next_trains et all_stations

--- FRONT/js/all_stations.js
+++ FRONT/js/all_stations.js
@@ -1,10 +1,9 @@
 var parameters = location.search.substring(1).split("=");
-<<<<<<< HEAD
+
 town = unescape(parameters[1]);
-=======
-city = unescape(parameters[1]);
-document.getElementById("town").innerHTML = city
->>>>>>> 0d530d7b7e789633725b0b139744f60ddddbe7e6
+
+document.getElementById("town").innerHTML = town
+
 
 const app = document.getElementById('root')
 
@@ -18,11 +17,7 @@ var request = new XMLHttpRequest()
 request.responseType = 'json'
 // Open a new connection, using the GET request on the URL endpoint
 
-<<<<<<< HEAD
 request.open('GET', "http://127.0.0.1:5000/"+town+"/stations", true)
-=======
-request.open('GET', "http://127.0.0.1:5000/"+city+"/stations", true)
->>>>>>> 0d530d7b7e789633725b0b139744f60ddddbe7e6
 
 
 request.onload = function () {
@@ -31,60 +26,33 @@ request.onload = function () {
   console.log(data)
 
   if (request.status >= 200 && request.status < 400) {
-<<<<<<< HEAD
 
-     // Create a div with a card class
-     const card = document.createElement('div')
-     card.setAttribute('class', 'card')
- 
-     // Create an h1 and set the text content to the film's title
-     const h2 = document.createElement('h2')
-     h2.textContent = town
-     card.appendChild(h2)
+    // Create a div with a card class
+    const card = document.createElement('div')
+    card.setAttribute('class', 'card')
+
+    // Create an h1 and set the text content to the film's title
+    const h2 = document.createElement('h2')
+    h2.textContent = town
+    card.appendChild(h2)
      
-
     data.forEach((station) => {
-
-    const a = document.createElement('a')
-    a.setAttribute('href', 'next_trains.html?town='+town+ '&'+ 'station='+station)
-    a.setAttribute('id', station)
-    
-    // Create a p and set the text content to the film's description
-    const p = document.createElement('p')
-    p.textContent = station
-    
-
-    // Append the cards to the container element
-    container.appendChild(card)
-    
-    // Each card will contain an h1 and a p
-    a.appendChild(p)
-    card.appendChild(a)
-      
-=======
-      // Create a div with a card class
-      const card = document.createElement('div')
-      card.setAttribute('class', 'card')
-  
-      // Create an h1 and set the text content to the film's title
-      const h2 = document.createElement('h2')
-      h2.textContent = city
-      card.appendChild(h2)
-      const p = document.createElement('p')
-      data.forEach((station) => {
+      const a = document.createElement('a')
+      a.setAttribute('href', 'next_trains.html?town='+town+ '&'+ 'station='+station)
+      a.setAttribute('id', station)
       // Create a p and set the text content to the film's description
       const p = document.createElement('p')
       p.textContent = station
       // Append the cards to the container element
       container.appendChild(card)
       // Each card will contain an h1 and a p
-      card.appendChild(p)  
->>>>>>> 0d530d7b7e789633725b0b139744f60ddddbe7e6
+      a.appendChild(p)
+      card.appendChild(a)     
     })
   } else {
-  const errorMessage = document.createElement('marquee')
-  errorMessage.textContent = `Gah, it's not working!`
-  app.appendChild(errorMessage)
+    const errorMessage = document.createElement('marquee')
+    errorMessage.textContent = `Gah, it's not working!`
+    app.appendChild(errorMessage)
   }
 }
 

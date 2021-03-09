1c8c416467454d24f0b01d37b3455c2926d1c042
emilie-lb <emilielb75@gmail.com>
2021-03-09 19:36:02 +0100

    ajout de tests

--- test/test_create_db.py
+++ test/test_create_db.py
@@ -1,41 +1,159 @@
 import sqlite3
 import unittest
+from multitransport.create_db import *
 
 
-class TestFonctions(unittest.TestCase):
-    def test_liste_stations(self):
-        conn = sqlite3.connect(":memory:")
-        cursor = conn.cursor()
-        cursor.execute("""DROP TABLE IF EXISTS table_test""")
-        cursor.execute("""CREATE TABLE IF NOT EXISTS "table_test" (
-        "Ligne"	TEXT,
-        "Arrêt"	TEXT,
-        "Destination"	TEXT,
-        "Temps_attente"	INTEGER,
-        "Ville"	TEXT
-        );""")
-        cursor.execute(
+class TestCreateDb(unittest.TestCase):
+    
+    # def set_up(self):
+    #     conn = sqlite3.connect(":memory:")
+    #     c = conn.cursor()
+    #     create_schema(c)
+    #     c.execute(
+    #         """
+    #         SELECT * FROM 'info_trafic';
+    #         """)
+
+    #     with open('./test/TAM_test.csv', "r", encoding="utf-8") as f:
+    #         f.readline()
+    #         line = f.readline()
+    #         while line:
+    #             insert_csv_row(line.strip().split(';'), c)
+    #             line = f.readline()
+
+
+    def test_create_schema(self):
+        # set_up(self)
+        conn, c = connect(":memory:")
+        create_schema(c)
+        c.execute(
             """
-            SELECT * FROM 'table_test';
+            SELECT * FROM 'info_trafic';
             """)
-        self.assertEqual(cursor.fetchone(), None)
-        with open('TAM_test.csv', "r", encoding="utf-8") as f:
-            f.readline()
-            line = f.readline()
-            while line:
-                cursor.execute("""
-                INSERT INTO table_test VALUES (?,?,?,?,?)
-                """, line.strip().split(";"))
-                line = f.readline()
+        self.assertEqual(c.fetchone(), None)
         conn.commit()
-        cursor.execute(
-            """
-            SELECT * FROM 'table_test';
-            """)
-        self.assertEqual(
-            cursor.fetchone(),
-            ('1', 'ANTIGONE', 'MOSSON', '21:05:23', 'Montpellier')
-            )
+        conn.close()
+
+
+    def test_clear_rows(self):
+        conn, c = connect(":memory:")
+        create_schema(c)
+        c.execute("""INSERT INTO info_trafic VALUES (?,?,?,?,?) """,
+                   ('1', 'test1', 'test1', '12:00:00', 'Montpellier'))
+        c.execute("""INSERT INTO info_trafic VALUES (?,?,?,?,?) """,
+                   ('2', 'test2', 'test2', '14:00:00', 'Montpellier'))
+        clear_rows(c, "Montpellier")
+        # tester la longueur du tableau pour vérifier qu'il est égal à 0
+        self.assertEqual(c.fetchone(), None)
+
+    def test_insert_csv_row(self):
+        conn, c = connect(":memory:")
+        create_schema(c)
+        row1 = ['3', 'test3', 'test3', '16:30:00', "Montpellier"]
+        insert_csv_row(row1, c)
+        requete = c.execute("""SELECT * FROM info_trafic WHERE ligne = '3'""")
+        self.assertEqual(c.fetchone(),('3', 'test3', 'test3', '16:30:00', 'Montpellier'))
+
+
+    # def test_load_csv(self):
+    #     conn, c = connect(":memory:")
+
+
+    def test_remove_table(self):
+        conn, c = connect(":memory:")
+        create_schema(c)
+        c.execute("""INSERT INTO info_trafic VALUES (?,?,?,?,?) """,
+                   ('1', 'test1', 'test1', '12:00:00', 'Montpellier'))
+        c.execute("""INSERT INTO info_trafic VALUES (?,?,?,?,?) """,
+                   ('2', 'test2', 'test2', '14:00:00', 'Montpellier'))
+        remove_table(c)
+        self.assertEqual(c.fetchone(), None)
+        # test = c.execute(
+        #     """
+        #     SELECT * FROM 'info_trafic';
+        #     """)
+        # try:
+        #     c.execute("""SELECT * 
+        #          FROM info_trafic""")
+        #     print('table existe')
+        # except: 
+        #     print("table n'existe pas")
+        # self.assertEqual(c.fetchone(),('1', 'test1', 'test1', '12:00:00', 'Montpellier'))
+        
+        # try:
+        #     c.execute("""SELECT * 
+        #          FROM info_trafic""")
+        #     print('table existe')
+        # except: 
+        #     print("table n'existe pas")
+        
+                 
+                 
+        # f c.fetchone()[0]==1 : 
+        #     print('Table exists.')
+        # else :
+        #     print('Table does not exist.')
+        # remove_table(c)
+        # # if c.fetchone()[0]==1 : {
+        # #     print('Table exists.')
+        # # }
+        # self.assertEqual(c.fetchone(), None)
+
+
+    # def test_liste_stations(self):
+    #     conn, c = connect(":memory:")
+    #     self.connection = sqlite3.connect(":memory:")
+    #     create_schema(c)
+    #     c.execute(
+    #         """
+    #         SELECT * FROM 'info_trafic';
+    #         """)
+    #     self.assertEqual(c.fetchone(), None)
+    #     with open('./test/TAM_test.csv', "r", encoding="utf-8") as f:
+    #         f.readline()
+    #         line = f.readline()
+    #         while line:
+    #             insert_csv_row(line.strip().split(';'), c)
+    #             line = f.readline()
+    #     c.execute(
+    #         """
+    #         SELECT * FROM 'info_trafic';
+    #         """)
+    #     self.assertEqual(
+    #         c.fetchone(),
+    #         ('1', 'ANTIGONE', 'MOSSON', '21:05:23', 'Montpellier')
+    #         )
+
+    #     # clear_rows(c, "Montpellier")
+    #     # self.assertEqual(c.fetchone(), None)
+    #     result = c.execute(
+    #         """
+    #         SELECT exists (select * from info_trafic);
+    #         """).fetchall()
+    #     self.assertEqual(result, [(1,)])
+    #     remove_table(c)
+
+
+        
+
+        # conn.close()
+        # with open('./test/TAM_test.csv', "r", encoding="utf-8") as f:
+        #     f.readline()
+        #     line = f.readline()
+        #     while line:
+        #         cursor.execute("""
+        #         INSERT INTO table_test VALUES (?,?,?,?,?)
+        #         """, line.strip().split(";"))
+        #         line = f.readline()
+        # conn.commit()
+        # cursor.execute(
+        #     """
+        #     SELECT * FROM 'table_test';
+        #     """)
+        # self.assertEqual(
+        #     cursor.fetchone(),
+        #     ('1', 'ANTIGONE', 'MOSSON', '21:05:23', 'Montpellier')
+        #     )
 
 
 if __name__ == '__main__':
--- test/test_download_csv.py
+++ test/test_download_csv.py
@@ -1,43 +1,30 @@
-import urllib.request
-import os
-import unittest
+# import urllib.request
+# import os
+# import unittest
+# from multitransport.download_csv import download
 
-class DownloadCsv(unittest.TestCase):
-    # check response status is 200
-    def test_download_MTP(self):
-        url = 'https://data.montpellier3m.fr/sites/default/files/ressources' \
-                '/TAM_MMM_TpsReel.csv'
-        dl = "test_TpsReel_MPL.csv"
-        urllib.request.urlretrieve(url, dl)
-        # actual_file_path = app.config.get('BASE_DIR') + '/static/uploads/' + filename
-        # self.assertEqual(file_path, actual_file_path)
-        self.assertTrue(os.path.exists(dl))
+# class DownloadCsv(unittest.TestCase):
+#     # check response status is 200
+#     def test_download_MTP(self):
+#         dl = download("Montpellier")
+#         self.assertTrue(os.path.exists(dl))
+#         os.remove(dl) 
 
-    # def test_download_RNS(self):
-    #     url = 'https://data.rennesmetropole.fr/explore/dataset/prochains-pa' \
-    #             'ssages-des-lignes-de-metro-du-reseau-star-en-temps-reel/do' \
-    #             'wnload/?format=csv&timezone=Europe/Berlin&lang=fr&use_labe' \
-    #             'ls_for_header=true&csv_separator=%3B'
-    #     dl = "test_TpsReel_RNS.csv"
-    #     urllib.request.urlretrieve(url, dl)
-    #     self.assertTrue(os.path.exists(dl))
+#     def test_download_RNS(self):
+#         dl = download("Rennes")
+#         self.assertTrue(os.path.exists(dl))
+#         os.remove(dl) 
 
-    # def test_download_LIL(self):
-    #     url = 'https://opendata.lillemetropole.fr/explore/dataset/ilevia-pr' \
-    #             'ochainspassages/download/?format=csv&timezone=Europe/Paris' \
-    #             '&lang=fr&use_labels_for_header=true&csv_separator=%3B'
-    #     dl = "test_TpsReel_LIL.csv"
-    #     urllib.request.urlretrieve(url, dl)
-    #     self.assertTrue(os.path.exists(dl))
+#     # def test_download_LIL(self):
+#     #     dl = download("Lille")
+#     #     self.assertTrue(os.path.exists(dl))
+#     #     os.remove(dl) 
 
-    # def test_download_ANE(self):
-    #     url = 'https://data.angers.fr/explore/dataset/bus-tram-circulation-' \
-    #             'passages/download/?format=csv&timezone=Europe/Berlin&lang=' \
-    #             'fr&use_labels_for_header=true&csv_separator=%3B'
-    #     dl = "test_TpsReel_ANE.csv"
-    #     urllib.request.urlretrieve(url, dl)
-    #     self.assertTrue(os.path.exists(dl))
+#     # def test_download_ANE(self):
+#     #     dl = download("Angers")
+#     #     self.assertTrue(os.path.exists(dl))
+#     #     os.remove(dl) 
 
 
-if __name__ == "__main__":
-    unittest.main()
+# if __name__ == "__main__":
+#     unittest.main()

5f9e9d0594df558c7af18b47a345f3c33a5b147c
Adrian Ruiz <adrianruizmora@hotmail.com>
2021-04-13 10:05:20 +0200

    modif de demarrage conteneurs

--- FRONT/js/all_stations.js
+++ FRONT/js/all_stations.js
@@ -17,7 +17,7 @@ var request = new XMLHttpRequest()
 request.responseType = 'json'
 // Open a new connection, using the GET request on the URL endpoint
 
-request.open('GET', "http://10.0.4.5:5000/"+town+"/stations", true)
+request.open('GET', "http://0.0.0.0:5000/"+town+"/stations", true)
 
 
 request.onload = function () {
--- run.py
+++ run.py
@@ -6,4 +6,5 @@ import multitransport.app as app
 
 
 if __name__ == '__main__':
+    # app.app.run(debug=True)
     app.app.run(host="0.0.0.0", port="5000", debug=True)
